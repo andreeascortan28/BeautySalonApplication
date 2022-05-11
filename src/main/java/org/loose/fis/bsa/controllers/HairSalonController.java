dca56be552ead6747ba134b32825073437ae4bae
andreeascortan28 <olivera.scortan@student.upt.ro>
2022-05-11 14:48:38 +0300

    Modified Facilities Controlles

--- src/main/java/org/loose/fis/bsa/controllers/HairSalonController.java
+++ src/main/java/org/loose/fis/bsa/controllers/HairSalonController.java
@@ -1,4 +1,19 @@
 package org.loose.fis.bsa.controllers;
-
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import java.awt.*;
 public class HairSalonController {
+    @FXML
+    private Button backField;
+
+    public void handleInapoiAction() throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("customerPage.fxml"));
+        Parent root = loader.load();
+        Stage stage = (Stage) (backField.getScene().getWindow());
+        stage.setScene(new Scene(root,600,400));
+    }
 }
